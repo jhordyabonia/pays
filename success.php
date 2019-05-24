7f934c96ee4a0689f00703186d6dac4a98ebf39d
Jhordy Abonia <jeigl7@gmail.com>
2019-05-24 17:15:49 -0500

    Working Success

--- success.php
+++ success.php
@@ -2,7 +2,8 @@
 
 require_once("./JSON_DB/JSON_DB.php");
 $database = new JSON_DB('payu-pays');
-$database->update(['referenceCode'=>$_POST['referenceCode']],$_POST);
+$database->update(['referenceCode'=>$_GET['referenceCode']],$_GET);
+$database->commit();
 #header("Location: http://www.linkedin.com/in/jhordy-abonia");
 $html =<<<HTML
 <div align="center" styly="margin:15px">
